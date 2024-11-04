9081d2f55f05aaa6f1ca24197b7d602f72a57a66
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-04 22:26:47 +0100

    first commit

---
+++ src/main/java/com/Florent/model/USER_ROLE.java
@@ -0,0 +1,8 @@
+package com.Florent.model;
+
+public enum USER_ROLE {
+
+    CUSTOMER,
+    RESTAURANT_OWNER,
+    ADMIN
+}
---
+++ src/test/java/com/Florent/OnlineFoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Florent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
