5c62ec3c47b7fea51697e46d6da547c02d98cf1e
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-04 23:14:25 +0100

    Order Entity added

--- src/main/java/com/Florent/model/Order.java
+++ src/main/java/com/Florent/model/Order.java
@@ -1,14 +1,40 @@
 package com.Florent.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @ManyToOne
+    private User customer;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+    private Long totalAmount;
+    private String orderStatus;
+    private Date createAt;
+
+    @ManyToOne
+    private Address deliveryAddress;
+
+    @OneToMany
+    private List<OrderItem> items;
+    private int totalItem;
+    private int totalPrice;
+
 }
