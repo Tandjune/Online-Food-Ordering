40800683952e9c1c458c5f1f8f1488baf4366d8b
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-05 13:25:09 +0100

    UserRepository created

---
+++ src/main/java/com/Florent/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.Florent.repository;
+
+import com.Florent.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByEmail(String username);
+}
