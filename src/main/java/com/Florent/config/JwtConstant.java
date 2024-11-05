42d6487b546991f36eb76ff2b4651e8189370b90
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-05 13:50:42 +0100

    TokenValidator added

---
+++ src/main/java/com/Florent/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.Florent.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY = "random&dkajsfj,jasdjfjkah@hjdfhahfiduash&5%76w8r%jaks?ajdhfkas%jafhdk-fjkashdf.hafjk";
+
+    public static final String JWT_HEADER = "Authorization";
+
+}
