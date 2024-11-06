78344ac1abda44130c1c4416574fe0cfcaac8194
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-06 12:15:41 +0100

    AuthResponse added

---
+++ src/main/java/com/Florent/response/AuthResponse.java
@@ -0,0 +1,10 @@
+package com.Florent.response;
+
+import com.Florent.model.USER_ROLE;
+
+public class AuthResponse {
+
+    private String jwt;
+    private String message;
+    private USER_ROLE role;
+}
--- src/main/java/com/Florent/service/CustomerUserDetailsService.java
+++ src/main/java/com/Florent/service/CustomerUserDetailsService.java
@@ -19,7 +19,6 @@ public class CustomerUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
