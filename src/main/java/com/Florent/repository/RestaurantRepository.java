02492b7e441d6fa64686175078abfa00badfb321
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-23 15:31:44 +0100

    Bug fix in RestaurantRepository

--- src/main/java/com/Florent/repository/RestaurantRepository.java
+++ src/main/java/com/Florent/repository/RestaurantRepository.java
@@ -8,8 +8,9 @@ import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
-    @Query("SELECT r from Restaurant r where lower(r.name) LIKE lower(concat('%', :query, '%') ) " + "or lower(r.cuisineType) like lower(concat('%', :query, '%') )")
+    @Query("SELECT r from Restaurant r where lower(r.name) LIKE lower(concat('%', :query, '%') ) " +
+            "or lower(r.cuisineType) like lower(concat('%', :query, '%') )")
     List<Restaurant> findBySearchQuery(String query);
 
-    Restaurant finByOwnerId(Long userId);
+    Restaurant findByOwnerId(Long userId);
 }
