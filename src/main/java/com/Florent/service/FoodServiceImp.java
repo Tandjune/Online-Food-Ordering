0b7a6e444d48b22b8f791b09a4d4b717e835ce89
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-24 22:40:34 +0100

    FoodServiceImp added

---
+++ src/main/java/com/Florent/service/FoodServiceImp.java
@@ -0,0 +1,111 @@
+package com.Florent.service;
+
+import com.Florent.model.Category;
+import com.Florent.model.Food;
+import com.Florent.model.Restaurant;
+import com.Florent.repository.FoodRepository;
+import com.Florent.request.CreateFoodRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class FoodServiceImp implements FoodService {
+
+    @Autowired
+    private FoodRepository foodRepository;
+
+    @Override
+    public Food createFood(CreateFoodRequest req, Category category, Restaurant restaurant) {
+        Food food = new Food();
+        food.setFoodCategory(category);
+        food.setRestaurant(restaurant);
+        food.setDescription(req.getDescription());
+        food.setImages(req.getImage());
+        food.setName(req.getName());
+        food.setPrice(req.getPrice());
+        food.setIngredients(req.getIngredients());
+        food.setSeasonal(req.isSeasonal());
+        food.setVegetarian(req.isVegetarian());
+
+        Food savedFood = foodRepository.save(food);
+        restaurant.getFoods().add(savedFood);
+
+        return savedFood;
+    }
+
+    @Override
+    public void deleteFood(Long foodId) throws Exception {
+        Food food = findFoodById(foodId);
+        food.setRestaurant(null);
+        foodRepository.save(food);
+    }
+
+    @Override
+    public List<Food> getRestaurantsFood(Long restaurantId, boolean isVegetarian, boolean isNonveg, boolean isSeasonal, String foodCategory) {
+        List<Food> foods = foodRepository.findByRestaurantId(restaurantId);
+
+        if (isVegetarian) {
+            foods = filterByVegetarian(foods);
+        }
+        if (isNonveg) {
+            foods = filterByNonveg(foods);
+        }
+        if (isSeasonal) {
+            foods = filterBySeasonal(foods);
+        }
+        if (foodCategory != null && !foodCategory.isEmpty()) {
+            foods = filterByCategory(foods, foodCategory);
+        }
+
+        return foods;
+    }
+
+    private List<Food> filterByCategory(List<Food> foods, String foodCategory) {
+        return foods.stream().filter(food -> {
+            if (food.getFoodCategory() != null) {
+                return food.getFoodCategory().getName().equals(foodCategory);
+            }
+            return false;
+        }).collect(Collectors.toList());
+    }
+
+    private List<Food> filterBySeasonal(List<Food> foods) {
+        return foods.stream().filter(food -> food.isSeasonal()).collect(Collectors.toList());
+    }
+
+    private List<Food> filterByNonveg(List<Food> foods) {
+        return foods.stream().filter(food -> !food.isVegetarian()).collect(Collectors.toList());
+    }
+
+    private List<Food> filterByVegetarian(List<Food> foods) {
+        return foods.stream().filter(Food::isVegetarian).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Food> searchFood(String keyword) {
+        return foodRepository.searchFood(keyword);
+    }
+
+    @Override
+    public Food findFoodById(Long foodId) throws Exception {
+        Optional<Food> food = foodRepository.findById(foodId);
+
+        if (food.isEmpty()) {
+            throw new Exception("No food found");
+        }
+
+        return food.get();
+    }
+
+    @Override
+    public Food updateAvailabilityStatus(Long foodId) throws Exception {
+        Food food = findFoodById(foodId);
+        food.setAvailable(!food.isAvailable());
+
+        return foodRepository.save(food);
+    }
+}
