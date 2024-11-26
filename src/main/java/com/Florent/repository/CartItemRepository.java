90ac74771e70f5d5544a55ae594339e8907d3664
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-26 23:54:54 +0100

    CartItemRepository added

---
+++ src/main/java/com/Florent/repository/CartItemRepository.java
@@ -0,0 +1,7 @@
+package com.Florent.repository;
+
+import com.Florent.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
