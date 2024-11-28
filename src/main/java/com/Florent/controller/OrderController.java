6e7075db00c32c558ab69a3a09837083e64ad9b9
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-28 02:49:24 +0100

    OrderController added

---
+++ src/main/java/com/Florent/controller/OrderController.java
@@ -0,0 +1,40 @@
+package com.Florent.controller;
+
+import com.Florent.model.Order;
+import com.Florent.model.User;
+import com.Florent.request.OrderRequest;
+import com.Florent.service.OrderService;
+import com.Florent.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/order")
+    public ResponseEntity<Order> createOrder(@RequestBody OrderRequest req, @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+        Order order = orderService.createOrder(req, user);
+        return new ResponseEntity<>(order, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/order/user")
+    public ResponseEntity<List<Order>> getOrderHistory(@RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        List<Order> orders = orderService.getUsersOrder(user.getId());
+
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+}
