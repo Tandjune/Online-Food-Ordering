152eb9a8bfa7a4835915fcf6fb4a311df691a09d
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-28 02:47:17 +0100

    OrderService added

---
+++ src/main/java/com/Florent/service/OrderService.java
@@ -0,0 +1,22 @@
+package com.Florent.service;
+
+import com.Florent.model.Order;
+import com.Florent.model.User;
+import com.Florent.request.OrderRequest;
+
+import java.util.List;
+
+public interface OrderService {
+
+    public Order createOrder(OrderRequest order, User user) throws Exception;
+
+    public Order updateOrder(Long orderId, String orderStatus) throws Exception;
+
+    public void cancelOrder(Long orderId) throws Exception;
+
+    public List<Order> getUsersOrder(Long userId);
+
+    public List<Order> getRestaurantsOrder(Long restaurantId, String orderStatus);
+
+    public Order findOrderById(Long orderId) throws Exception;
+}
