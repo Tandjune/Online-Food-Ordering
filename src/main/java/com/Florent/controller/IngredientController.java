c6b67ed9758a938bfad0056fae14a9d402c4c271
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-29 21:12:33 +0100

    ingredient api tested

--- src/main/java/com/Florent/controller/IngredientController.java
+++ src/main/java/com/Florent/controller/IngredientController.java
@@ -2,11 +2,10 @@ package com.Florent.controller;
 
 import com.Florent.model.IngredientCategory;
 import com.Florent.model.IngredientsItem;
-import com.Florent.model.User;
 import com.Florent.request.IngredientCategoryRequest;
 import com.Florent.request.IngredientItemRequest;
+import com.Florent.response.MessageResponse;
 import com.Florent.service.IngredientService;
-import com.Florent.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,44 +20,46 @@ public class IngredientController {
     @Autowired
     private IngredientService ingredientService;
 
-    @Autowired
-    private UserService userService;
-
     @PostMapping("/category")
-    public ResponseEntity<IngredientCategory> createIngredientCategory(@RequestBody IngredientCategoryRequest req, @RequestHeader("Authorization") String jwt) throws Exception {
-        User user = userService.findUserByJwtToken(jwt); // added, was empty
+    public ResponseEntity<IngredientCategory> createIngredientCategory(@RequestBody IngredientCategoryRequest req) throws Exception {
         IngredientCategory ingredientCategory = ingredientService.createIngredientCategory(req.getName(), req.getRestaurantId());
 
         return new ResponseEntity<>(ingredientCategory, HttpStatus.CREATED);
     }
 
     @PostMapping()
-    public ResponseEntity<IngredientsItem> createIngredientItem(@RequestBody IngredientItemRequest req, @RequestHeader("Authorization") String jwt) throws Exception {
-        User user = userService.findUserByJwtToken(jwt); // added, was empty
+    public ResponseEntity<IngredientsItem> createIngredientItem(@RequestBody IngredientItemRequest req) throws Exception {
         IngredientsItem ingredientItem = ingredientService.createIngredientItem(req.getRestaurantId(), req.getIngredientName(), req.getCategoryId());
 
         return new ResponseEntity<>(ingredientItem, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}/stoke")
-    public ResponseEntity<IngredientsItem> updateIngredientStoke(@PathVariable Long id, @RequestHeader("Authorization") String jwt) throws Exception {
-        User user = userService.findUserByJwtToken(jwt); // added, was empty
+    public ResponseEntity<IngredientsItem> updateIngredientStoke(@PathVariable Long id) throws Exception {
         IngredientsItem ingredientItem = ingredientService.updateStock(id);
 
         return new ResponseEntity<>(ingredientItem, HttpStatus.OK);
     }
 
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<MessageResponse> deleteIngredientItem(@PathVariable Long id) throws Exception {
+        ingredientService.deleteIngredientItem(id);
+
+        MessageResponse message = new MessageResponse();
+        message.setMessage("Ingredient item deleted successfully!");
+
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
     @GetMapping("/restaurant/{id}")
-    public ResponseEntity<List<IngredientsItem>> getRestaurantIngredient(@PathVariable Long id, @RequestHeader("Authorization") String jwt) throws Exception {
-        User user = userService.findUserByJwtToken(jwt); // added, was empty
+    public ResponseEntity<List<IngredientsItem>> getRestaurantIngredient(@PathVariable Long id) throws Exception {
         List<IngredientsItem> ingredientItems = ingredientService.findRestaurantsIngredients(id);
 
         return new ResponseEntity<>(ingredientItems, HttpStatus.OK);
     }
 
     @GetMapping("/restaurant/{id}/category")
-    public ResponseEntity<List<IngredientCategory>> getRestaurantIngredientCategory(@PathVariable Long id, @RequestHeader("Authorization") String jwt) throws Exception {
-        User user = userService.findUserByJwtToken(jwt); // added, was empty
+    public ResponseEntity<List<IngredientCategory>> getRestaurantIngredientCategory(@PathVariable Long id) throws Exception {
         List<IngredientCategory> ingredientItemCategories = ingredientService.findIngredientCategoryByRestaurantId(id);
 
         return new ResponseEntity<>(ingredientItemCategories, HttpStatus.OK);
